fc6e60b43c3dc7bab83666bbed4d57faffbbbb80
Minzhi <li.minzhi@dhs.sg>
2020-03-17 08:40:13 +0800

    firebase modified

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,15 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    //Key in your database details
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: ""
-  };
+  apiKey: "AIzaSyDgrE_3NmDxEp7Zw8jtObEfNV3pLcKZcl4",
+  authDomain: "bt3103lec.firebaseapp.com",
+  databaseURL: "https://bt3103lec.firebaseio.com",
+  projectId: "bt3103lec",
+  storageBucket: "bt3103lec.appspot.com",
+  messagingSenderId: "876146062133",
+  appId: "1:876146062133:web:c8e93b582c57a93a75927c",
+  measurementId: "G-6DE5CQQLTB"
+};
   
   firebase.initializeApp(firebaseConfig);
   var database = firebase.firestore();
